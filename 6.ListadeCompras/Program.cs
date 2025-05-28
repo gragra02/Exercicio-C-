623f2f735e9640436ead7c9e93c600a3ebaa8817
gragra <gragra02@gmail.com>
2025-05-28 20:19:40 -0300

    Envio do exercicio 6

---
+++ 6.ListadeCompras/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> lista = new List<string>();
+        Console.WriteLine("Digite os itens da lista (digite 'sair' para finalizar):");
+        string item = "";
+
+        while (item != "sair")
+        {
+            item = Console.ReadLine();
+            if (item != "sair")
+                lista.Add(item);
+        }
+
+        foreach (string i in lista)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

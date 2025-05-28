f0716dfd629a0b618398397bd73cd9a068d2c8bf
gragra <gragra02@gmail.com>
2025-05-28 20:51:54 -0300

    exercicio 7

---
+++ 7. ContadordeVogais/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite uma palavra");
+string palavra = Console.ReadLine();
+
+int quantidadeLetras = palavra.Length;
+Console.WriteLine($"A palavra {palavra} tem ({quantidadeLetras}) Letras");
+
+
+
+

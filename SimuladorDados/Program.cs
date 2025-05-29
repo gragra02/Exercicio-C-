32c20cab7bb5230d577ad1538899f9769b1d8764
gragra <gragra02@gmail.com>
2025-05-28 21:17:10 -0300

    exercicio simulador de dado

---
+++ SimuladorDados/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite rolar para rolar o dado:");
+Console.ReadLine(); 
+Random geradordeNumero = new Random();
+int resultado = geradordeNumero.Next(1, 7);
+Console.WriteLine($"O número sorteado foi: {resultado}");
